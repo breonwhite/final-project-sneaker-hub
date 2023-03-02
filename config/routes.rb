20951768cd9a8a46656f235c6cf914535a7f8b69
Breon White <bdwhite92@gmail.com>
2023-03-01 20:55:39 -0500

    Add controller for Users

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
+  resources :users, only: [:index, :show, :create, :update, :destroy]
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
