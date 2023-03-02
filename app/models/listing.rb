a12ae6657beab2a5a4e3b8d859b88bbfb298851a
Breon White <bdwhite92@gmail.com>
2023-03-01 20:54:57 -0500

    Add active record associations for all models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,6 @@
 class Listing < ApplicationRecord
+    belongs_to :seller, class_name: 'User'
+    belongs_to :buyer, class_name: 'User', optional: true
+    belongs_to :sneaker
+    has_one :purchase
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :listings, foreign_key: :seller_id
+    has_many :purchases, foreign_key: :buyer_id
 end
