8ed73893d8ce6ae5540ad362afe2012873e244ab
Breon White <bdwhite92@gmail.com>
2023-03-03 00:07:13 -0500

    Add routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Account from './components/Account';
+import Listing from './components/Listing';
+import Listings from './components/Listings';
+import Profile from './components/Profile';
+import Signup from './components/Signup';
+import Purchases from './components/Purchases';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Routes>
+      <Route exact path="/" element={ <Home /> } />
+      <Route exact path="/signup" element={ <Signup /> } />
+      <Route exact path="/login" element={ <Login /> } />
+      <Route exact path="/listings" element={ <Listings /> } />
+      <Route exact path="/listings/:id" element={ <Listing /> } />
+      <Route exact path="/purchases" element={ <Purchases /> } />
+      <Route exact path="/account/:id" element={ <Account /> } />
+      <Route exact path="/profile/:id" element={ <Profile /> } />
+      <Route exact path="/shop" element={ <Shop /> } />
+     </Routes>
     </div>
   );
 }
