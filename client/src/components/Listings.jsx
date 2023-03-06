bb538dc67c0e455225369dfe1a9d367a595b1708
Breon White <bdwhite92@gmail.com>
2023-03-06 14:45:47 -0500

    Show all listings on listings

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { UserContext } from '../context/User';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListingsItem from '../containers/listings/ListingsItem';
+
 
 const Listings = () => {
+  const { user, listings } = useContext(UserContext); // Listing context from User.jsx
+
+  const currentUserListings = listings.filter(l => l.seller_id === user.id && l.sold === false);
+  const otherListings = listings.filter(l => l.seller_id !== user.id && l.sold === false);
+  
+  
   return (
-    <div>Listings</div>
+    <div>
+      <h1>Listings</h1><br/>
+      <h2>My Current Listings</h2><br/>
+      <ul>
+      { currentUserListings.map((l, index) => 
+        <li key={ index }>{l.price}, {l.size}, {l.sneaker.name}</li> 
+      )}
+      </ul>
+      <br/>
+      <h2>Current Listings</h2><br/>
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          { otherListings.map((l, index) => 
+          <div key={ index }>
+            <ListingsItem listing={l} />
+            <Divider />
+            </div>
+          )}
+        </List>
+      {/* <ul>
+        { otherListings.map((l, index) => 
+          <li key={ index }>{l.price}, {l.size}, {l.sneaker.name}</li> 
+        )}
+        </ul> */}
+      
+
+
+
+    </div>
   )
 }
 
