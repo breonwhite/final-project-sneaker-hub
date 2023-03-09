25234409eaa8ea9c8d914203918345d901db3ad9
Breon White <bdwhite92@gmail.com>
2023-03-08 23:21:43 -0500

    Update Listing serializer to include sneakers

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -4,4 +4,10 @@ class ListingSerializer < ActiveModel::Serializer
   belongs_to :seller, class_name: 'User'
   belongs_to :buyer, class_name: 'User', optional: true
   belongs_to :sneaker
+
+  def attributes(*args)
+    data = super
+    data[:sneaker] = self.object.sneaker.slice(:id, :name, :colorway, :retail_price, :image)
+    data
+  end
 end
