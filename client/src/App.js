f4d45d6c05e174ef4d1bb5401077b7fbb90a7175
Breon White <bdwhite92@gmail.com>
2023-03-12 23:59:21 -0400

    Clean up App.js file by adding Lazy

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,35 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom'
-import Home from './components/Home';
-import Account from './components/Account';
-import Listing from './components/Listing';
-import Listings from './components/Listings';
-import Profile from './components/Profile';
+import { UserProvider } from './context/User';
 import Signup from './components/Signup';
-import Purchases from './components/Purchases';
+import Login from './components/Login';
+import Navbar from './containers/Navbar';
 import './App.css';
 
 function App() {
+
+  const Home = React.lazy(() => import('./components/Home'));
+  const Dashboard = React.lazy(() => import('./components/Dashboard'));
+  const Account = React.lazy(() => import('./components/Account'));
+  const Listings = React.lazy(() => import('./components/Listings'));
+  const ListingNew = React.lazy(() => import('./components/ListingNew'));
+
   return (
     <div className="App">
-     <Routes>
-      <Route exact path="/" element={ <Home /> } />
-      <Route exact path="/signup" element={ <Signup /> } />
-      <Route exact path="/login" element={ <Login /> } />
-      <Route exact path="/listings" element={ <Listings /> } />
-      <Route exact path="/listings/:id" element={ <Listing /> } />
-      <Route exact path="/purchases" element={ <Purchases /> } />
-      <Route exact path="/account/:id" element={ <Account /> } />
-      <Route exact path="/profile/:id" element={ <Profile /> } />
-      <Route exact path="/shop" element={ <Shop /> } />
-     </Routes>
+    <UserProvider>
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}> 
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+        <Route exact path="/signup" element={ <Signup /> } />
+        <Route exact path="/login" element={ <Login /> } />
+        <Route exact path="/listings" element={ <Listings /> } />
+        <Route exact path="/listings/new" element={ <ListingNew /> } />
+        <Route exact path="/dashboard" element={ <Dashboard /> } />
+        <Route exact path="/account" element={ <Account /> } /> 
+      </Routes>
+      </Suspense>
+    </UserProvider>
     </div>
   );
 }
