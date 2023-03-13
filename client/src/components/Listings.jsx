d0c80a98cbcbc530e2749f5c0631aaf8d675f93a
Breon White <bdwhite92@gmail.com>
2023-03-12 23:05:41 -0400

    Clean up Listings Page

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -4,52 +4,24 @@ import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListingsItem from "../containers/listings/ListingsItem";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import Button from "@mui/material/Button";
-import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import ContactSupportIcon from "@mui/icons-material/ContactSupport";
-import TodayIcon from "@mui/icons-material/Today";
-import MapIcon from "@mui/icons-material/Map";
 import PurchaseDialog from "../containers/listings/PurchaseDialog";
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-
-
-import { useTheme } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import SkipNextIcon from "@mui/icons-material/SkipNext";
-import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
-import Paper from "@mui/material/Paper";
-
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import Unauthorized from "../containers/Unauthorized";
 import Loading from "../containers/Loading";
 
 const Listings = () => {
-  const { user, listings, purchaseListing, fetchListings, loggedIn, loading } = useContext(UserContext); // Listing context from User.jsx
-  const [ activeListings, setActiveListings ] = useState([]);
-
+  const { user, listings, purchaseListing, loggedIn, loading } =
+    useContext(UserContext); // Listing context from User.jsx
+  const [activeListings, setActiveListings] = useState([]);
 
   useEffect(() => {
-    const filteredListings = listings.filter((l) => l.seller_id !== user.id && l.sold === false);
+    const filteredListings = listings.filter(
+      (l) => l.seller_id !== user.id && l.sold === false
+    );
     setActiveListings(filteredListings);
   }, [listings]);
-
 
   const handlePurchase = async (listing) => {
     try {
@@ -93,46 +65,61 @@ const Listings = () => {
   } else if (!loggedIn) {
     return <Unauthorized />;
   } else {
-  return (
-    <div>
-      <Container>
-      <Typography variant="h3" align="left" gutterBottom>
-        Soles for Sale { activeListings? `(${activeListings.length})` : ""}
-      </Typography>
-      <Box sx={{ width: '100%' }}>
-      <Grid container sx={{ width: '100%' }}>
-      <List sx={{ width: 5000, bgcolor: "background.paper" }}>
-        {activeListings.map((l, index) => {
-          return (
-            <div key={index}>
-              <ListingsItem
-                listing={l}
-                confirmPurchase={() => confirmPurchase(l, index)}
-              />
-              <Divider />
-              <Dialog
-                open={isDialogOpen && openDialogIndex === index}
-                onClose={handleClose}
-                aria-labelledby="confirm-purchase-title"
-                aria-describedby="alert-dialog-description"
-              >
-                <PurchaseDialog user={user} l={l} handleClose={handleClose} handlePurchase={handlePurchase} />
-                
-              </Dialog>
-            </div>
-          );
-        })}
-      </List>
-      {/* <ul>
-        { otherListings.map((l, index) => 
-          <li key={ index }>{l.price}, {l.size}, {l.sneaker.name}</li> 
-        )}
-        </ul> */}
+    return (
+      <Box
+        style={{
+          backgroundSize: "cover",
+          height: "100vh",
+          justifyContent: "center",
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ width: "80%", margin: "auto" }}
+        >
+          <Typography variant="h3" align="center" gutterBottom>
+            Soles for Sale ({activeListings ? `${activeListings.length}` : ""})
+          </Typography>
+          <Grid container style={{ width: "100%", margin: "auto" }}>
+            {activeListings.length == 0 ? (
+              "Looks like there are no active listings right now. Check back later."
+            ) : (
+              <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+                {activeListings.map((l, index) => {
+                  return (
+                    <div key={index}>
+                      <ListingsItem
+                        listing={l}
+                        confirmPurchase={() => confirmPurchase(l, index)}
+                      />
+                      <Divider />
+                      <Dialog
+                        open={isDialogOpen && openDialogIndex === index}
+                        onClose={handleClose}
+                        aria-labelledby="confirm-purchase-title"
+                        aria-describedby="alert-dialog-description"
+                      >
+                        <PurchaseDialog
+                          user={user}
+                          l={l}
+                          handleClose={handleClose}
+                          handlePurchase={handlePurchase}
+                        />
+                      </Dialog>
+                    </div>
+                  );
+                })}
+              </List>
+            )}
+          </Grid>
         </Grid>
-        </Box>
-        </Container>
-    </div>
-  )};
+      </Box>
+    );
+  }
 };
 
 export default Listings;
