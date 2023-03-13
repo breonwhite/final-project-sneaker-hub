8b82f2e48f4e46c29ca36255b8bd03c302d79a20
Breon White <bdwhite92@gmail.com>
2023-03-12 23:42:51 -0400

    Clean up Listing Dialog

--- client/src/containers/dashboard/listing/ListingDialog.jsx
+++ client/src/containers/dashboard/listing/ListingDialog.jsx
@@ -1,38 +1,20 @@
 import React, { useState, useContext } from "react";
 import { UserContext } from "../../../context/User";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import List from "@mui/material/List";
 import InputAdornment from "@mui/material/InputAdornment";
 import Chip from "@mui/material/Chip";
-
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-
-import Container from "@mui/material/Container";
-import { useTheme } from "@mui/material/styles";
-import Tab from "@mui/material/Tab";
-import MobileStepper from "@mui/material/MobileStepper";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import Divider from "@mui/material/Divider";
-import SwipeableViews from "react-swipeable-views";
-import { autoPlay } from "react-swipeable-views-utils";
 import Typography from "@mui/material/Typography";
-
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
-const ListingDialog = ({ l, handleClose, updateListing }) => {
-    const { listings, editListing, setListings } = useContext(UserContext); // Login context from User.jsx
-
+const ListingDialog = ({ l, handleClose, updateListing, deleteListing }) => {
   const [listing, setListing] = useState({
     price: l.price,
     size: l.size,
@@ -49,6 +31,11 @@ const ListingDialog = ({ l, handleClose, updateListing }) => {
     event.preventDefault();
     console.log("hitting hadle submit");
     updateListing(l, listing);
+  };
+
+  const handleDelete = () => {
+    console.log("hitting handle delete");
+    deleteListing(l);
   };
 
   return (
@@ -129,7 +116,7 @@ const ListingDialog = ({ l, handleClose, updateListing }) => {
         <Button size="small" onClick={handleClose}>
           Cancel
         </Button>
-        <Button size="small" onClick={handleClose}>
+        <Button size="small" onClick={handleDelete}>
           Delete Listing
         </Button>
         <Button type="submit" size="large" variant="contained" color="success">
