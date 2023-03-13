533d09846621df4e75a74d3f2eb7553f49710b2b
Breon White <bdwhite92@gmail.com>
2023-03-12 23:38:06 -0400

    Clean up Account View Page

--- client/src/containers/account/AccountView.jsx
+++ client/src/containers/account/AccountView.jsx
@@ -1,9 +1,4 @@
 import React, { useState, useContext } from "react";
-import { UserContext } from "../../context/User";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
@@ -16,99 +11,99 @@ import CardActions from "@mui/material/CardActions";
 const AccountView = ({ user, showForm }) => {
   return (
     <form>
-        <Typography variant="h2" gutterBottom>
-              Your Account
-            </Typography>
-            <Card sx={{ width: "100%", padding: "10px" }} elevation={24}>
-              <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
-                <Stack direction="row" spacing={2}>
-                  <TextField
-                    sx={{ width: "100%" }}
-                    disabled
-                    name="first_name"
-                    label="FIRST NAME"
-                    defaultValue={user.first_name}
-                  />
-                  <TextField
-                    sx={{ width: "100%" }}
-                    disabled
-                    name="last_name"
-                    label="LAST NAME"
-                    defaultValue={user.last_name}
-                  />
-                </Stack>
-              </FormControl>
-              <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
-                <TextField
-                  disabled
-                  fullWidth
-                  name="username"
-                  label="USERNAME"
-                  defaultValue={user.username}
-                />
-              </FormControl>
-              <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
-                <TextField
-                  sx={{ width: "100%" }}
-                  disabled
-                  name="address"
-                  label="ADDRESS"
-                  defaultValue={user.address}
-                />
-              </FormControl>
-              <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
-                <TextField
-                  sx={{ width: "100%" }}
-                  disabled
-                  name="city"
-                  label="CITY"
-                  defaultValue={user.city}
-                />
-              </FormControl>
-              <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
-                <Stack direction="row" spacing={2}>
-                  <TextField
-                    sx={{ width: "100%" }}
-                    disabled
-                    name="state"
-                    label="STATE"
-                    defaultValue={user.state}
-                  />
-                  <TextField
-                    sx={{ width: "100%" }}
-                    disabled
-                    type="number"
-                    name="zipcode"
-                    label="ZIPCODE"
-                    defaultValue={user.zipcode}
-                  />
-                </Stack>
-              </FormControl>
-              <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
-                <TextField
-                  disabled
-                  fullWidth
-                  name="phone_number"
-                  label="PHONE NUMBER"
-                  defaultValue={user.phone_number}
-                />
-              </FormControl>
-              <CardActions>
-                <Button
-                onClick={() => showForm()}
-                  variant="contained"
-                  size="normal"
-                  style={{
-                    backgroundColor: "#212121",
-                    fontSize: "15px",
-                  }}
-                >
-                  Edit Profile
-                </Button>
-              </CardActions>
-            </Card>
-          </form>
-  )
-}
+      <Typography variant="h2" gutterBottom>
+        Your Account
+      </Typography>
+      <Card sx={{ width: "100%", padding: "10px" }} elevation={24}>
+        <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
+          <Stack direction="row" spacing={2}>
+            <TextField
+              sx={{ width: "100%" }}
+              disabled
+              name="first_name"
+              label="FIRST NAME"
+              defaultValue={user.first_name}
+            />
+            <TextField
+              sx={{ width: "100%" }}
+              disabled
+              name="last_name"
+              label="LAST NAME"
+              defaultValue={user.last_name}
+            />
+          </Stack>
+        </FormControl>
+        <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
+          <TextField
+            disabled
+            fullWidth
+            name="username"
+            label="USERNAME"
+            defaultValue={user.username}
+          />
+        </FormControl>
+        <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
+          <TextField
+            sx={{ width: "100%" }}
+            disabled
+            name="address"
+            label="ADDRESS"
+            defaultValue={user.address}
+          />
+        </FormControl>
+        <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
+          <TextField
+            sx={{ width: "100%" }}
+            disabled
+            name="city"
+            label="CITY"
+            defaultValue={user.city}
+          />
+        </FormControl>
+        <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
+          <Stack direction="row" spacing={2}>
+            <TextField
+              sx={{ width: "100%" }}
+              disabled
+              name="state"
+              label="STATE"
+              defaultValue={user.state}
+            />
+            <TextField
+              sx={{ width: "100%" }}
+              disabled
+              type="number"
+              name="zipcode"
+              label="ZIPCODE"
+              defaultValue={user.zipcode}
+            />
+          </Stack>
+        </FormControl>
+        <FormControl fullWidth sx={{ width: "100%", mt: 2, mr: 1 }}>
+          <TextField
+            disabled
+            fullWidth
+            name="phone_number"
+            label="PHONE NUMBER"
+            defaultValue={user.phone_number}
+          />
+        </FormControl>
+        <CardActions>
+          <Button
+            onClick={() => showForm()}
+            variant="contained"
+            size="normal"
+            style={{
+              backgroundColor: "#212121",
+              fontSize: "15px",
+            }}
+          >
+            Edit Profile
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+  );
+};
 
-export default AccountView
+export default AccountView;
