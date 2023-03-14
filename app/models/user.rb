ad99ecc32275ec261bbdeb288ca7ff9acb24c46d
Breon White <bdwhite92@gmail.com>
2023-03-13 22:38:07 -0400

    Add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :first_name, :last_name, :email, :password, presence: true
     
     has_many :listings, foreign_key: :seller_id
     has_many :purchases, foreign_key: :buyer_id
